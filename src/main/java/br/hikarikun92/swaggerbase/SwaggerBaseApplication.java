8a2f4ab9255eb92257b7b6b943f65824d496a57d
Lucas Souza <lucas.souza@monitoratec.com.br>
2020-06-26 12:50:40 -0300

    Initial commit - basic structure

---
+++ src/main/java/br/hikarikun92/swaggerbase/SwaggerBaseApplication.java
@@ -0,0 +1,13 @@
+package br.hikarikun92.swaggerbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerBaseApplication.class, args);
+    }
+
+}
